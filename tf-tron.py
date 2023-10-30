4de0775745afab526bf1c6064d45248dbf9c8de5
Feby Zamsee <febyzamsee@gmail.com>
2023-10-30 08:51:23 +0700

    upload create trx wallet & tf trx

---
+++ tf-tron.py
@@ -0,0 +1,74 @@
+from pprint import pprint
+import hashlib
+import requests
+import base58
+import ecdsa
+from Crypto.Hash import keccak
+import requests as r
+
+url = 'https://api.trongrid.io'
+def keccak256(data):
+	hasher = keccak.new(digest_bits=256)
+	hasher.update(data)
+	return hasher.digest()
+
+
+def verifying_key_sendto(key):
+	pubKey = key.to_string()
+	primitive_addr = b'\x41' + keccak256(pubKey)[-20:]
+	addr = base58.b58encode_check(primitive_addr)
+	return addr
+
+def ceksaldo(wallet):
+	result = r.get(f"https://apilist.tronscanapi.com/api/accountv2?address={wallet}")
+	saldo = result.json()["withPriceTokens"][0]["amount"]
+	saldo = float(saldo)
+	return saldo
+
+
+def main(file="wallet.txt"):
+	for  wlPK in open(file,'r').read().strip().splitlines():
+		resText = ''
+		privKey = wlPK.split(' ')[1]
+		raw_priv_key = bytes.fromhex(privKey)
+		priv_key = ecdsa.SigningKey.from_string(raw_priv_key, curve=ecdsa.SECP256k1)
+		pubKey = priv_key.get_verifying_key().to_string()
+		primitive_addr = b'\x41' + keccak256(pubKey)[-20:]
+		addr = base58.b58encode_check(primitive_addr)
+		resText += f" [{addr.decode()}]"
+		saldo = ceksaldo(addr.decode())
+		resText += f" {saldo:.3f} TRX"
+		if saldo >= 0.7:
+			jumlah = round(int(saldo * 1000000 - 500))
+			# jumlah = 1000000
+			resText += ' | '+'SENDING'
+			transaction = {
+				"to_address": base58.b58decode_check(sendto).hex(),
+				"owner_address": primitive_addr.hex(),
+				"amount": jumlah,
+			}
+			resp = requests.post(url + '/wallet/createtransaction', json=transaction)
+			payload = resp.json()
+			if 'message' in payload:
+				resText += ' | '+str(bytes.fromhex(payload['message']).decode())
+			raw_data = bytes.fromhex(payload['raw_data_hex'])
+			signature = priv_key.sign_deterministic(raw_data, hashfunc=hashlib.sha256)
+			pubKeys = ecdsa.VerifyingKey.from_public_key_recovery(
+				signature[:64], raw_data, curve=ecdsa.SECP256k1, hashfunc=hashlib.sha256
+			)
+			for v, pk in enumerate(pubKeys):
+				if verifying_key_sendto(pk) == addr:
+					break
+			signature += bytes([v])
+			payload['signature'] = [signature.hex()]
+			resp = requests.post(url + '/wallet/broadcasttransaction', json=payload)
+			result = resp.json()
+			if 'message' in result:
+				resText += ' | '+str(bytes.fromhex(result['message']).decode())
+			else:
+				resText += ' | '+str(result)
+		print(resText)
+
+if __name__ == '__main__':
+	sendto = input(' [ ? ] Send to Wallet > ')
+	main()
---
+++ trx.py
@@ -0,0 +1,68 @@
+import ecdsa
+from ecdsa.curves import SECP256k1
+from ecdsa.keys import SigningKey
+from Crypto.Hash import keccak,SHA256
+from base58 import b58encode as b58
+import random
+import secrets
+import time
+import base58
+
+
+def save(pk,wl,file="tron-wallets.txt"):
+	with open(file,'a') as f:
+		f.write(f"{wl} {pk}\n")
+
+def generateBytes(length=64):
+	if length % 2 != 0:
+		raise ValueError("Length must be an even number")
+	random_bytes = secrets.token_bytes(length // 2)
+	private_key_hex = random_bytes.hex()
+	return private_key_hex
+
+def kecc256(data):
+	res = keccak.new(digest_bits=256)
+	res.update(bytes.fromhex(data))
+	return res.hexdigest()
+
+def sha256(data):
+	res = SHA256.new()
+	res.update(bytes.fromhex(data))
+	return res.hexdigest()
+
+def wallet(privKey_hex: str):
+	privKey = SigningKey.from_string(bytes.fromhex(privKey_hex), curve=SECP256k1)
+	pubKey = privKey.get_verifying_key().to_string(encoding="uncompressed").hex()
+	pub = pubKey[2:]
+	ketja = kecc256(pub)
+	kec = ketja[24:]
+	stepsis = '41'+kec
+	sh1 = sha256(stepsis)
+	sh2 = sha256(sh1)
+	cksum = sh2[0:8]
+	lolita = stepsis+cksum
+	result = b58(bytes.fromhex(lolita)).decode()
+	return result
+
+def plaintoAddress(data):
+	decoded_bytes = base58.b58decode(data)
+	hex_str = decoded_bytes.hex()
+	return hex_str[0:-8]
+
+def create(jumlah:int,submain=None):
+	st = time.time()
+	for _ in range(jumlah):
+		pk=generateBytes()
+		if submain is not None:
+			pk = kecc256(submain+pk)
+		wl=wallet(pk)
+		save(pk,wl,'subwalet.txt')
+	fn = time.time()
+	print(f"Complete in: {fn-st:.2f}")
+
+
+if __name__ == '__main__':
+	try:
+	   create(int(input(' [ ? ] Jumlah Wallet TRX > ')))
+	except:
+		exit('Error...')
